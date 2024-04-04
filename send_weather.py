fe16c981e8fca5d5ba024fcfc37ce17394da9eaf
Freddie Wilder <wildernonstop@gmail.com>
2024-04-04 20:18:34 +0700

    fix

--- send_weather.py
+++ send_weather.py
@@ -2,8 +2,7 @@ import bot
 import schedule
 import os
 
-cur_dir = os.getcwd()
-db_file = cur_dir + "/subscribers.txt"
+db_file = os.path.abspath('subscribers.txt')
 print(db_file)
 schedule.every().minute.do(lambda: bot.send_weather(db_file))
 print("Запуск скрипта рассылки..")
