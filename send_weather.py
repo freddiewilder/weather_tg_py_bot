5f50f9761bbf4b24088f7ca9d6782ed74661ec49
Freddie Wilder <wildernonstop@gmail.com>
2024-04-04 19:03:18 +0700

    fix for linux

--- send_weather.py
+++ send_weather.py
@@ -1,8 +1,11 @@
 import bot
 import schedule
+import os
 
-
-schedule.every().minute.do(lambda: bot.send_weather('subscribers.txt'))
+cur_dir = os.getcwd()
+db_file = cur_dir + "/subscribers.txt"
+print(db_file)
+schedule.every().minute.do(lambda: bot.send_weather(db_file))
 print("Запуск скрипта рассылки..")
 while True:
     schedule.run_pending()
